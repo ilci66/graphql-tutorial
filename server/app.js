228270766a53f668eb059e92d7b9ba51671cc8f7
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-13 16:47:25 +0300

    database connection added

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // the naming of the file is apparently is just conventional
 // const graphqlHTTP = require('express-graphql');
 // had to deconstruct, was trowing an error, the tutorial I'm 
 // following showed it as above, but the bottom one worked for me  
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {}).then(() => console.log('Connected to database')) 
 
 const app = express();
 
