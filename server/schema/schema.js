b8985621c9ee1bd2704fe188d150e957d0f54f52
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-13 15:58:54 +0300

    notes on GraphQLList

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,19 +8,22 @@ const {
     GraphQLObjectType, 
     GraphQLString, 
     GraphQLSchema,
-    // no need id to be a string anymore in queries, it will appear as string when it's querying
     GraphQLID,
-    // for integers
-    GraphQLInt 
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 
 // dummy data
 var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
+
 var authors = [
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
     { name: 'Brandon Sanderson', age: 42, id: '2' },
@@ -28,20 +31,51 @@ var authors = [
 ];
 
 // create the schemas
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    // when fields is a fucntion didn't throw any 'undefined' errors so just keep it in mind
+    // that fields is supposed to be a fucntion 
+    fields: () => ({
+        id: {type:GraphQLID},
+        name: {type:GraphQLString},
+        age: {type: GraphQLInt},
+        book: {
+            // as an author may have multiple books 'GraphQLList' should be used
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                // the parent here is author object and authorId is already present
+                return _.filter(books, { authorId: parent.id})
+            }
+        }
+    })
+})
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: {type:GraphQLID},
         name: {type:GraphQLString},
-        genre: {type:GraphQLString}
-    })
-})
-const AuthorType = new GraphQLObjectType({
-    name: 'Author',
-    fields: () => ({
-        id: {type:GraphQLID},
-        name: {type:GraphQLString},
-        age: {type: GraphQLInt}
+        genre: {type:GraphQLString},
+        // we can define the relations between shcmemas
+        author: {
+            // type of data I'm expecting
+            type: AuthorType,
+            // resolve is the method that actually looks for and serves the data
+            // parent in this case is the book object
+            resolve(parent, args){
+                // console.log(parent)
+                return _.find(authors, { id: parent.authorId})
+                // {
+                //     book(id:2){
+                //       name
+                //       genre
+                //       author{
+                //         name
+                //       }
+                //     }
+                //   }
+                // this kind of a query is now possible
+            }
+        }
     })
 })
 
