920408a5066709d9b29b20199858c081900c87a1
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-14 12:32:06 +0300

    add required logic with GraphQLNonNull

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,7 +12,8 @@ const {
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 
@@ -140,8 +141,8 @@ const Mutation = new GraphQLObjectType({
             type: AuthorType,
             // the necessary arguments to create an author
             args: {
-                name: { type: GraphQLString },
-                age: { type: GraphQLInt }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
             },
             resolve(parent, args){
                 let author = new Author({
@@ -165,9 +166,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString },
-                genre: { type:  GraphQLString },
-                authorId: { type: GraphQLID }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLID) }
             },
             resolve(parent, args){
                 let book = new Book({
