1352e178675371744370de2542afbbab495c7787
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-14 14:46:12 +0300

    apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import BookList from "./components/BookList";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+
+// now apollo knows we will make request to this enpoint from this component
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
-    <div id="main">
-      <h1>Ilker's Reading List</h1>
-      <BookList />
-    </div>
+    // we need to wrap it with the provider in order to use apollo
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ilker's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
+    
   );
 }
 
